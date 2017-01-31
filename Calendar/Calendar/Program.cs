5ffdbcf0ef1544230c7d3e8ebbcc7dc5fada0f8e
Vihar Sanghavi <vihar@promact.com>
2017-01-31 18:26:08 +0530

    my changes

---
+++ Calendar/Calendar/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+
+
+namespace Calendar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int year, leapCnt, nonLeapCnt, day, month, date, remainDay;
+                string temp;
+                bool leap = false, complete = false;
+                int[] monthCnt = new int[] { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+                again:
+                Console.Write("Enter year: ");
+                temp = Console.ReadLine();
+                if (DigitsOnly(temp) == false)         //Check year contains only digits or not
+                {
+                    Console.WriteLine("Year should contains only digits.");
+                    goto again;
+                }
+                year = Convert.ToInt32(temp);
+                if (year < 1900 || year > 2030)         //Check year is in range or not
+                {
+                    Console.WriteLine("Year should be in between 1900 to 2030");
+                    goto again;
+                }
+                again1:
+                Console.Write("Enter month number: ");
+                temp = Console.ReadLine();
+                if (DigitsOnly(temp) == false)         //Check month contains only digits or not
+                {
+                    Console.WriteLine("Month should contains only digits.");
+                    goto again1;
+                }
+                month = Convert.ToInt32(temp);
+                if (month < 1 || month > 12)            //Check month is in range or not
+                {
+                    Console.WriteLine("Month should be in between 1 to 12");
+                    goto again1;
+                }
+
+                if (year % 100 == 0)                //Check year is century year or not 
+                {
+                    if (year % 400 == 0)            //Check year is century leap year or not 
+                    {
+                        leap = true;
+                    }
+                }
+                else if (year % 4 == 0)             //Check year is leap year or not 
+                {
+                    leap = true;
+                }
+                if (leap == true)
+                {
+                    monthCnt[1] = 29;               //Change no of days of feb month due to leap year 
+                }
+                leapCnt = (year - 1) / 4;           //Count total leap year 
+                nonLeapCnt = (year - 1) - leapCnt;  //Count total non-leap year
+                day = ((leapCnt * 2) + nonLeapCnt);
+                for (int i = 0; i < month - 1; i++)
+                {
+                    day = day + monthCnt[i];        //Count total extra days
+                }
+                day = day % 7;
+                remainDay = 7 - day;
+                Console.WriteLine("Sun\tMon\tTue\tWed\tThu\tFri\tSat");
+                date = 1;
+                while (day > 0)
+                {
+                    Console.Write("\t");
+                    day--;
+                }
+                for (int i = 0; i < remainDay; i++)
+                {
+                    Console.Write(" " + date + "\t");
+                    date++;
+                }
+                Console.WriteLine("");
+                while (complete != true)
+                {
+                    for (int i = 0; i < 7; i++)
+                    {
+                        if (date < 10)                 //For right justification 
+                        {
+                            Console.Write(" " + date + "\t");
+                        }
+                        else
+                        {
+                            Console.Write(date + "\t");
+                        }
+
+                        date++;
+                        if (date > monthCnt[month - 1])
+                        {
+                            complete = true;
+                            break;
+                        }
+                    }
+                    Console.WriteLine("");
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Error..");
+            }
+            Console.ReadKey();
+
+        }
+
+        static bool DigitsOnly(string str)
+        {
+            foreach (char c in str)
+            {
+                if (c < '0' || c > '9')
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Making_Palindrome/Making_Palindrome/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+using System.Linq;
+
+namespace Making_Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string str;
+                int start = 0, end, b = 0;
+                bool palr = false, print = false;
+                char[] buff = new char[100];
+                Console.Write("Enter String: ");
+                str = Console.ReadLine();
+                char[] str1 = str.ToCharArray();
+                char[] reverse = str.ToCharArray();
+                Array.Reverse(reverse);
+                if (Enumerable.SequenceEqual(str1, reverse) == true) //Check whether string is palindrome or not
+                {
+                    Console.WriteLine("String is already Palindrome");
+                }
+                else
+                {
+                    char[] pal = new char[str1.Length];
+                    end = str1.Length - 1;
+                    for (int i = 0; i < str1.Length; i++)
+                    {
+                        palr = false;
+                        if (str1[i] == ' ')
+                        {
+                            continue;
+                        }
+
+                        for (int j = i + 1; j < str1.Length; j++)
+                        {
+                            if (str1[i] == str1[j])  //Comparing with other characters
+                            {
+                                pal[start++] = str1[i];
+                                pal[end--] = str1[j];
+                                str1[j] = ' ';  // Removing character from array
+                                palr = true;
+                                break;
+
+                            }
+                        }
+                        if (palr == false) //Check character has one occurance or not
+                        {
+                            buff[b++] = str1[i];
+                        }
+                        if (b >= 2)    //Check no of characters with one occurance 
+                        {
+                            Console.WriteLine("String can't be palindrome");
+                            print = true;
+                            break;
+                        }
+
+                    }
+                    if (b == 1)
+                    {
+                        pal[start] = buff[0];    // Copy buffer character to main array 
+                    }
+                    if (print == false)
+                    {
+                        string temp = new string(pal);
+                        Console.Write("Palindrome String: " + temp);
+
+                    }
+
+
+
+
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Error...");
+            }
+            Console.ReadKey();
+        }
+           
+        
+    }
+}
